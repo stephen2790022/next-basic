28cb66db8417b64d94563dbe0c526d1d9e810a72
stephen2790022 <toulouse.stephen1.com>
2021-03-19 11:25:25 +0100

    ajout du contenue sur la page items

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Layout } from '../../components/layout'
-
+import Head from 'next/head'
 const Items = () => {
   return (
-    <Layout>
-      <h1>Items</h1>
-    </Layout>
+    <>
+      <Head>
+        <title>Items</title>
+      </Head>
+      <Layout>
+        <h1>Items</h1>
+        <ul>
+          <h2>Liste des categories</h2>
+          <li>Culture</li>
+          <li>Géographie</li>
+          <li>Social</li>
+          <li>Economique</li>
+        </ul>
+      </Layout></>
   )
 }
 
